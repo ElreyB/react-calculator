8bceef212157faadee75d694919b0ccacee08432
Elrey Belmonti <elbelmonti@gmail.com>
2018-07-27 00:53:26 -0700

    style input container

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -2,7 +2,9 @@ import React, { Fragment } from 'react';
 
 const TextInput = ({ height, width }) => (
   <Fragment>
-    <div style={{ height, width }} className="input" />
+    <div style={{ height, width }} className="input">
+      <span className="input-span">25+25</span>
+    </div>
   </Fragment>
 );
 
